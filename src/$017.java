3772ed70b02199e5d3a49f134ab360506745531c
sfn <safefatnoob@gmail.com>
2017-07-27 23:25:23 +0500

    prob a better way

---
+++ src/$017.java
@@ -0,0 +1,21 @@
+
+
+    // Number letter counts
+    // https://projecteuler.net/problem=17
+    // the king is back lmao rip me
+
+
+public class $017
+{
+    public static void main(String args[])
+    {
+        int one[] = new int[]{3, 3};
+        int two[] = new int[]{3, 6};
+        int three[] = new int[]{5, 6};
+        int four[] = new int[]{4, 6};
+        int five[] = new int[]{4, 6};
+        int six[] = new int[]{3, 5};
+        int seven[] = new int[]{5, 7};
+
+    }
+}
