70c8e9646f9353198ee16fb1a259658b88ac63d3
sfn <safefatnoob@gmail.com>
2017-07-28 00:07:27 +0500

    fuckin fourty
    ya lol i cheated

--- src/$017.java
+++ src/$017.java
@@ -5,17 +5,41 @@
     // the king is back lmao rip me
 
 
-public class $017
+    import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+    public class $017
 {
     public static void main(String args[])
     {
-        int one[] = new int[]{3, 3};
-        int two[] = new int[]{3, 6};
-        int three[] = new int[]{5, 6};
-        int four[] = new int[]{4, 6};
-        int five[] = new int[]{4, 6};
-        int six[] = new int[]{3, 5};
-        int seven[] = new int[]{5, 7};
+        int numbers[][] = new int[][]{
+                {0, 0},
+                {3, 0},
+                {3, 6},
+                {5, 6},
+                {4, 5},
+                {4, 5},
+                {3, 5},
+                {5, 7},
+                {5, 6},
+                {4, 6}};
 
+        int onetonine = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            onetonine += numbers[i][0];
+        }
+
+        int tens = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            tens += numbers[i][1];
+        }
+
+        int oneto99 = (onetonine * 9) + (tens * 10) + 70;
+
+        int total = (oneto99 * 10) + (onetonine * 100) + (7 * 900) + (3 * 891) + (11);
+
+        System.out.println("a" + tens);
+        System.out.println("o" + total);
     }
 }
